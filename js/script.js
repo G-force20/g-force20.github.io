00fb2e1d4c306f992957d918620dc3dfe3847b98
Gabe Masso <gbm224@lehigh.edu>
2023-05-04 00:35:04 -0400

    looks like (JSON)'s back on the menu bois

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-    $.getJSON("js/data.json", (data) => {
+    $.getJSON("https://g-force20.github.io/js/marker.json", (data) => {
         console.log("read")
         console.log(data)
     });
