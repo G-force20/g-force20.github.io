c8964edcf37cc9fb2a961188d4af0e29a81d942f
G-force20 <89607907+G-force20@users.noreply.github.com>
2023-05-04 00:28:48 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-    $.getJSON("js/data.json", (data) => {
+    $.getJSON("https://g-force20.github.io/js/data.json", (data) => {
         console.log("read")
         console.log(data)
     });
