3936eb5b93233917e2c2da272ae69c13aeb33877
Gabe Masso <gbm224@lehigh.edu>
2023-05-04 00:56:44 -0400

    if this works imma lose my shit - copied from old repo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,71 @@
+// read data from json
+let data, map, shown;
+
 $(document).ready(() => {
-    $.getJSON("https://g-force20.github.io/js/marker.json", (data) => {
-        console.log("read")
-        console.log(data)
+    $.getJSON("https://g-force20.github.io/js/marker.json", (response) => {
+        // console.log("read")
+        // console.log(response)
+        data = response;
     });
 });
+
+// load google map API
+const loader = new google.maps.plugins.loader.Loader({
+    apiKey: "AIzaSyClit8oNyG7-uK0tOxGAgX9imqLrWm3RPc",
+    version: "weekly",
+    libraries: ["maps"]//, "marker"]
+});
+let load = loader.load().then(initMap);
+
+
+function initMap() {
+    // let steps = { lat: 40.608381759451525, lng: -75.37842078555052 }
+    // console.log(data.steps.pos)
+    map = new google.maps.Map(
+        document.getElementById("map"),
+        { zoom: 18, center: data[0].pos }
+    );
+    data.forEach((building) => {
+        building.clicked = false;
+        building.marker = new google.maps.Marker({ 
+            position: building.pos, 
+            map: map,
+            title: building.name
+         });
+        building.marker.addListener("click", () => {
+            // console.log(building.name+"clicked");
+            loadMarkers(building)
+        });
+    });
+}
+
+function loadMarkers(building) {
+    shown.forEach((point) => {
+        point.marker.setMap(null);
+    });
+
+    shown = building.points;
+    if(building.clicked){
+        //makes sure we don't have to initialize new markers if the building has been clicked on
+        // console.log("reused")
+        shown.forEach((point) => {
+            point.marker.setMap(map);
+        });
+    }
+    else{
+        // console.log("new")
+        shown.forEach((point) => {
+            // console.log(pic +" "+index);
+            point.marker = new google.maps.Marker({ position: point.pos, map: map });
+            point.marker.addListener("click", () => {
+                showAttributes(point);
+            });
+        
+        });
+        building.clicked=true;
+    }
+}
+
+function showAttributes(point){
+    console.log(point.img)
+}
